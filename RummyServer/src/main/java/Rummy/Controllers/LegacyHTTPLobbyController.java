89e50ff8634fa484f1f1927605fb313a5209291b
Hubert Janczak <hubert.janczaak@gmail.com>
2024-01-29 12:04:55 +0100

    Renames and deletions of legacy code

--- RummyServer/src/main/java/Rummy/Controllers/LegacyHTTPLobbyController.java
+++ RummyServer/src/main/java/Rummy/Controllers/LegacyHTTPLobbyController.java
@@ -1,14 +1,10 @@
 package Rummy.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * LEGACY
@@ -18,13 +14,13 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/lobby")
-public class LobbyController {
+public class LegacyHTTPLobbyController {
 
     /**
      * Instantiates a new LobbyController.
      */
     @Autowired
-    public LobbyController() {
+    public LegacyHTTPLobbyController() {
     }
 
     /**
